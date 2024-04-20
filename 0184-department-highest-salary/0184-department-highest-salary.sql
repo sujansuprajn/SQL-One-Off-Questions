fed60fd9672ac51da2dd5ea3258ca6fbfe3c1560
Sujan Supraj N <154021651+sujansuprajn@users.noreply.github.com>
2024-04-20 15:08:32 +0530

    Time: 1102 ms (88.83%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,2 @@
+with cte as (select d.name as Department, e.name as Employee, salary, dense_rank() over(partition by d.name order by salary desc) as rnk from employee e join department d on e.departmentId = d.id)
+select Department, Employee, Salary from cte where rnk = 1;
