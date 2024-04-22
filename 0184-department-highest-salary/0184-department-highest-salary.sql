e053bbf1edb7158eff87c4681a2efd23bdf521fc
Sujan Supraj N <154021651+sujansuprajn@users.noreply.github.com>
2024-04-22 21:51:38 +0530

    formatted

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,2 +1,14 @@
-with cte as (select d.name as Department, e.name as Employee, salary, dense_rank() over(partition by d.name order by salary desc) as rnk from employee e join department d on e.departmentId = d.id)
-select Department, Employee, Salary from cte where rnk = 1;
+with cte as 
+    ( select d.name as Department, 
+        e.name as Employee, salary, 
+        dense_rank() over(partition by d.name order by salary desc) as rnk 
+    from employee e 
+    join department d on e.departmentId = d.id )
+
+select 
+    Department, 
+    Employee, 
+    Salary 
+from 
+    cte 
+where rnk = 1;
