10dade48b7d41eb12a5c33ed4ad27b71c2dbacd0
Sujan Supraj N <154021651+sujansuprajn@users.noreply.github.com>
2024-04-23 22:53:54 +0530

    Time: 2987 ms (16.26%), Space: 0B (100.00%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,7 @@
+
+
+select user_id as buyer_id, join_date, count(o.buyer_id) as orders_in_2019 
+from users u left join orders o   on u.user_id = o.buyer_id and year(order_date) = '2019'
+group by u.user_id
+order by u.user_id
+
