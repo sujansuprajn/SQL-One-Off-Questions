cd771ab961c0708d26202ad9698a58d04fd08283
Sujan Supraj N <154021651+sujansuprajn@users.noreply.github.com>
2024-04-29 22:24:15 +0530

    Time: 1378 ms (22.39%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,6 @@
+
+select employee_id from salaries where employee_id not in (select employee_id from employees)
+union 
+select employee_id from employees where employee_id not in (select employee_id from salaries)
+
+order by employee_id
